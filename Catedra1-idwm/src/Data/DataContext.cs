e5b484e12054dacaa395b780edd4d0004b50b980
Yamir <yamcast12@gmail.com>
2024-10-02 20:03:09 -0300

    feat: agregado data context

---
+++ Catedra1-idwm/src/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using Catedra1_idwm.src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catedra1_idwm.src.Data
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Catedra1-idwm/src/Models/User.cs
+++ Catedra1-idwm/src/Models/User.cs
@@ -5,7 +5,7 @@ namespace Catedra1_idwm.src.Models
     public class User
     {
         [Key]
-        public string rut { get; set; } = string.Empty;
+        public string Rut { get; set; } = string.Empty;
 
         [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres")]
         public string Name { get; set; } = string.Empty;
