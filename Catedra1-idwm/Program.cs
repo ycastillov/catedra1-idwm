35e2c2e4acb9e4a6d658fe18afcb41a9e755f5fc
Yamir <yamcast12@gmail.com>
2024-10-02 20:29:49 -0300

    feat: configurado program

--- Catedra1-idwm/Program.cs
+++ Catedra1-idwm/Program.cs
@@ -1,11 +1,18 @@
 using Catedra1_idwm.src.Data;
+using Catedra1_idwm.src.Repositories;
+using Catedra1_idwm.src.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext> (options => options.UseSqlite("Data Source=Practica1.db"));
 
+builder.Services.AddControllers();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 using (var scope = app.Services.CreateScope()) // Se levanta la base de datos
 {
